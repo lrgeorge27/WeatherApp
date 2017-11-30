4fdedc9c550de05b91efdf1f0f4949f9d97c3d44
lrgeorge27 <lrgeorge27@gmail.com>
2017-11-30 13:04:24 +0000

    code for geolocater and api, neither work

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
-/* global $ APIKEY*/
+/* global $ APIKEY  */
 $(document).ready(function() {
-    $.ajax({
-        method: "GET",
-        url: " api.openweathermap.org.data/2.5/weather",
-        data: { apiKey: APIKEY },
-        success: function(data) {
-            if (data.status == "ok") {
-                // console.log(data);
-                // for (var i = 0; i < data.sources.length; i++) { //did not do sources[] because we are running through entire array
-                //     var source = document.createElement("OPTION");
-                //     source.setAttribute("value", data.sources[i].id)
-                //     source.innerHTML = data.sources[i].name;
-                //     document.getElementById("selection").appendChild(source);
-                //     console.log("display");
+    var location = {};
+    if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error);
+        console.log("start");
+    }
+    else {
+        alert("Geolocation unsupported");
+        console.log("stop");
+    }
 
-            }
-        }
+    function error() {
+        alert("Your location is unavailable.");
+        console.log(error);
+    }
 
-    });
+    function success() {
+        location.lat = position.coords.latitude;
+        location.lon = position.coords.longitude;
+        document.getElementById("latLong").innerHTML = location.lat + location.lon;
+        console.log(success);
+    }
+    // navigator.geolocation.getCurrentPosition(function(position) {
+    //     $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
 });
+
+var weather = "api.openweathermap.org.data/2.5/weather/lat=" + location.lat + "&lon=" + location.lon + "&appid=" + APIKEY;
+$.ajax({
+    method: "GET",
+    url: weather,
+    // "api.openweathermap.org.data/2.5/weather",
+    // data: { lat: location.lat, lon: location.lon, appid: APIKEY },
+    dataType: "json",
+    success: function(data) {
+        if (data.status == "ok") {
+            console.log(data);
+            // for (var i = 0; i < data.sources.length; i++) { //did not do sources[] because we are running through entire array
+            //     var source = document.createElement("OPTION");
+            //     source.setAttribute("value", data.sources[i].id)
+            //     source.innerHTML = data.sources[i].name;
+            //     document.getElementById("selection").appendChild(source);
+            //     console.log("display");
+
+        }
+    }
+});
+
+// });
+
+
 //     $("#source").submit(function(event) {
 //         event.preventDefault();
 //         var value = document.getElementById("selection").value;
